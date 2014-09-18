187806fc7ae3b39162a49adcb03c1c65de7c1003
lide <lide@lidematoMacBook-Pro-2.local>
2014-09-19 00:13:42 +0800

    create project

---
+++ YYMiOS/YYMiOS/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  YYMiOS
+//
+//  Created by lide on 14-9-18.
+//  Copyright (c) 2014年 Lide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ YYMiOS/YYMiOS/Controller/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  YYMiOS
+//
+//  Created by lide on 14-9-19.
+//  Copyright (c) 2014年 Lide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
